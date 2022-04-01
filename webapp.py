46ca8a77eb71663d0ad742ed142dd9319187a97f
Yharai <yharai5cm@gmail.com>
2022-04-01 09:56:41 -0700

    update

--- webapp.py
+++ webapp.py
@@ -1,24 +1,20 @@
 from flask import Flask, url_for, render_template, request
 
-app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+app = Flask(__webapp__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
 @app.route("/")
 def render_main():
     return render_template('index.html')
 
 @app.route("/response")
-def render_response():
-    color = request.args['color'] 
-    #The request object stores information about the request sent to the server.
-    #args is an ImmutableMultiDict (like a dictionary but can have mutliple values for the same key and can't be changed)
-    #The information in args is visible in the url for the page being requested. ex. .../response?color=blue
-    if color == 'pink':
-        reply1 = "That's my favorite color, too!"
-    else:
-        reply1 = "My favorite color is pink."
-    n = int(request.args['multNum']) #values in request.args are strings by default
-    reply2 = "2 x " + str(n) + " = " + str((2*n))
-    return render_template('response.html', response1 = reply1)
+def format_dict_as_graph_points(data):
+    graph_points = ""
+    for carbon in United States:
+        #{y: 2000, label: "CO2"}
+        graph_points = graph_points + Markup(' { y: ' to str(data[Emmissions{CO2}]) + ' { x: ' to str(data[Country{United States}]) + ', label:
+    graph_points = graph_points [:-2]
+    print(graph_points)
+    return render_template('carbon-increase.html', data = graph_points)
     
 if __name__=="__main__":
     app.run(debug=False)
