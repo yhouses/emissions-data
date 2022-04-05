e3351d480b49f2f6bafc6a77daee70f01d2fd5c7
Yharai <yharai5cm@gmail.com>
2022-04-05 08:45:30 -0700

    update

--- webapp.py
+++ webapp.py
@@ -1,6 +1,6 @@
 from flask import Flask, url_for, render_template, request
 
-app = Flask(__webapp__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
+app = Flask(__name__)
 
 @app.route("/")
 def render_main():
@@ -8,13 +8,15 @@ def render_main():
 
 @app.route("/response")
 def format_dict_as_graph_points(data):
+    with open('emissions.json') as carbon_increase:  
+        carbon = json.load(carbon_increase)
     graph_points = ""
-    for carbon in United States:
+    for data in United_States:
         #{y: 2000, label: "CO2"}
-        graph_points = graph_points + Markup(' { y: ' to str(data[Emmissions{CO2}]) + ' { x: ' to str(data[Country{United States}]) + ', label:
+        graph_points = graph_points + Markup(' { x: ' + str(data["Emissions"]["Type"]["C02"]) + ',  y: ' + str(data["Year"]) + '},')
     graph_points = graph_points [:-2]
     print(graph_points)
-    return render_template('carbon-increase.html', data = graph_points)
+    return render_template('carbon-increase.html', data = carbon_increase)
     
 if __name__=="__main__":
     app.run(debug=False)
